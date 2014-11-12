a4287750e0cee9bcbfebdd1933c855021421e15a
perfectconsulting <steven@perfectconsulting.co.uk>
2014-11-12 14:03:04 +0000

    Create rsa.hpp

---
+++ rsa.hpp
@@ -0,0 +1,22 @@
+#include "vlong.hpp"
+
+class public_key
+{
+  public:
+  vlong m,e;
+  vlong encrypt( const vlong& plain ); // Requires 0 <= plain < m
+};
+
+class private_key : public public_key
+{
+  public:
+  vlong p,q;
+  vlong decrypt( const vlong& cipher );
+
+  void create( const char * r1, const char * r2 );
+  // r1 and r2 should be null terminated random strings
+  // each of length around 35 characters (for a 500 bit modulus)
+};
+
+vlong from_str( const char * s );
+void to_str( vlong n, char * s);
